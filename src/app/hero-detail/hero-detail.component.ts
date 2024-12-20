e4ad7dd79e755aab093d827c98a411c0a374640f
tohobr <tohobr@lbst.dk>
2024-12-20 11:02:10 +0100

    test

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -46,20 +46,33 @@ export class HeroDetailComponent implements OnInit {
   //       .subscribe(() => this.goBack());
   //   }, 250, false)();
   // }
+
+  save(): void {
+    someThirdPartyPromise().then(() => {
+      this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+    });
+  }
 }
 
-// function debounce(func, wait, immediate) {
-//   var timeout;
-//   return function () {
-//     var context = this,
-//       args = arguments;
-//     var later = function () {
-//       timeout = null;
-//       if (!immediate) func.apply(context, args);
-//     };
-//     var callNow = immediate && !timeout;
-//     clearTimeout(timeout);
-//     timeout = setTimeout(later, wait);
-//     if (callNow) func.apply(context, args);
-//   };
-// }
+function someThirdPartyPromise() {
+  return new Promise((resolve) => {
+    resolve(null);
+  })
+}
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
